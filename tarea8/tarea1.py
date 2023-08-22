5c90bbba93b7e309432e2a11012c86c19e253d8a
Robert <robert.ramos.27@unsch.edu.pe>
2023-08-22 12:34:49 -0500

    finalmente listo

--- tarea8/tarea1.py
+++ tarea8/tarea1.py
@@ -1,5 +1,4 @@
 from Countries_data import countries
-
 #a) Crea una función llamada los idiomas más hablados en el mundo. 
 # Debería devolver los 10 o 20 idiomas más hablados en el mundo en orden descendente.
 from collections import Counter
@@ -13,7 +12,7 @@ def idiomas_mas_hablados_mundo(num_idiomas):
         contador_leguajes.update(lenguajes)
     lenguajes_mas_top = contador_leguajes.most_common(num_idiomas) #en aqui se uso most_common para obtener los elemtos mas comunes o los que tienen los mayores valores
     return lenguajes_mas_top
-
+#b) Crea una función llamada los países más poblados. Debería devolver los 10 o 20 países más poblados en orden descendente.
 top_idiomas = idiomas_mas_hablados_mundo(20)
 print("Los 20 idiomas más hablados del mundo segun los datos son:")
 for language, count in top_idiomas:
